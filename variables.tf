e11532f4beb1cf6c887ef91637eca2cc77b4573e
Ruben <rubenlopsol86@gmail.com>
2024-09-20 19:39:17 +0200

    viernes 20/09/24 19:30

--- variables.tf
+++ variables.tf
@@ -22,4 +22,74 @@ variable "department" {
 variable "region" {
   description = "Value for AWS region"
 }
-
+#! VPC
+variable "vpc-cidr-block" {
+  description = "Value for VPC CIDR block"
+  type        = string
+}
+variable "vpc-name" {
+  description = "Value for VPC name"
+  type        = string
+}
+#! Web subnets
+variable "web-subnet1-name" {
+  description = "Value for name of web subnet 1"
+  type        = string
+}
+variable "web-subnet2-name" {
+  description = "Value for name of web subnet 2"
+  type        = string
+}
+variable "web-subnet1-cidr" {
+  description = "Value for web subnet1 CIDR"
+  type        = string
+}
+variable "web-subnet2-cidr" {
+  description = "Value for web subnet2 CIDR"
+  type        = string
+}
+variable "az-1" {
+  description = "Value for AZ subnet 1"
+}
+variable "az-2" {
+  description = "Value for AZ subnet 2"
+}
+#! App subnets
+variable "app-subnet1-name" {
+  description = "Value for App subnet1 name"
+  type        = string
+}
+variable "app-subnet2-name" {
+  description = "Value for App subnet2 name"
+  type        = string
+}
+variable "app-subnet1-cidr" {
+  description = "Value for App subnet1 CIDR"
+  type        = string
+}
+variable "app-subnet2-cidr" {
+  description = "Value for App subnet2 CIDR"
+  type        = string
+}
+#! DB Subnets
+variable "db-subnet1-name" {
+  description = "Value for DB subnet1 name"
+  type        = string
+}
+variable "db-subnet2-name" {
+  description = "Value for DB subnet2 name"
+  type        = string
+}
+variable "db-subnet1-cidr" {
+  description = "Value for DB subnet1 CIDR"
+  type        = string
+}
+variable "db-subnet2-cidr" {
+  description = "Value for DB subnet2 CIDR"
+  type        = string
+}
+#! IGW
+variable "igw-name" {
+  description = "Value for Internet GateWay name"
+  type        = string
+}
